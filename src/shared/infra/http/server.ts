57e4e14e432341ed84545f7b3ffc25d87453d950
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-07-24 10:44:54 -0300

    feat: add server.ts

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
+
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(
+  (
+    error: Error,
+    request: Request,
+    response: Response,
+    nextFunction: NextFunction,
+  ) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+app.listen(port, () => {
+  console.log('******************************');
+  console.log(`server running on port ${port}`);
+  console.log('******************************');
+});
