3b11406cdb10100a24fc43b760ed7af7944a3e0f
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-07-28 22:54:15 -0300

    feat: add more tests to create contact

--- src/modules/contacts/services/CreateContactService.spec.ts
+++ src/modules/contacts/services/CreateContactService.spec.ts
@@ -1,22 +1,65 @@
-import ContactsRepository from '../infra/typeorm/repositories/ContactsRepository';
+import AppError from '@shared/errors/AppError';
+
+import FakeContactsRepository from '../repositories/Fake/FakeContactsRepository';
+import FakePhonesRepository from '../repositories/Fake/FakePhonesRepository';
 import CreateContactService from './CreateContactService';
 
-let contactsRepository: ContactsRepository;
+let fakeContactsRepository: FakeContactsRepository;
+let fakePhonesRepository: FakePhonesRepository;
 let createContactService: CreateContactService;
 
 describe('Create contact', () => {
   beforeEach(() => {
-    contactsRepository = new ContactsRepository();
-    createContactService = new CreateContactService(contactsRepository);
+    fakeContactsRepository = new FakeContactsRepository();
+    fakePhonesRepository = new FakePhonesRepository();
+    createContactService = new CreateContactService(
+      fakeContactsRepository,
+      fakePhonesRepository,
+    );
   });
 
   it('should be able to create a new contact', async () => {
-    const contact = await createContactService.execute({
+    const contact = {
       firstName: 'Emanuel',
       lastName: 'Pereira',
-      email: 'emanuelcdpr@gmail.com',
+      email: 'emanuelcd@gmail.com',
+      phones: '64999051758, 649996251072',
+    };
+    await createContactService.execute({
+      firstName: contact.firstName,
+      lastName: contact.lastName,
+      email: contact.email,
+      phones: contact.phones,
     });
 
-    expect(contact).toHaveProperty('id');
+    const createdContact = await fakeContactsRepository.findByEmail(
+      contact.email,
+    );
+
+    expect(createdContact).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new contact with already registered email', async () => {
+    const contact = {
+      firstName: 'Emanuel',
+      lastName: 'Pereira',
+      email: 'emanuelcd@gmail.com',
+      phones: '64999051758, 649996251072',
+    };
+    await createContactService.execute({
+      firstName: contact.firstName,
+      lastName: contact.lastName,
+      email: contact.email,
+      phones: contact.phones,
+    });
+
+    await expect(
+      createContactService.execute({
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+        email: contact.email,
+        phones: contact.phones,
+      }),
+    ).rejects.toEqual(new AppError('Contact already registered'));
   });
 });
