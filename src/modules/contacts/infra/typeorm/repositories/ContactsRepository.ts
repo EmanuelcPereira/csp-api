8ae0f54eccb0348d1ce3c42f5876cbab2b1a9892
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-07-28 22:55:22 -0300

    feat: add some find methods

--- src/modules/contacts/infra/typeorm/repositories/ContactsRepository.ts
+++ src/modules/contacts/infra/typeorm/repositories/ContactsRepository.ts
@@ -12,11 +12,13 @@ class ContactsRepository implements IContactsRepository {
   }
 
   public async create({
+    id,
     firstName,
     lastName,
     email,
   }: ICreateContactDTO): Promise<Contact> {
     const contact = this.repository.create({
+      id,
       firstName,
       lastName,
       email,
@@ -27,10 +29,35 @@ class ContactsRepository implements IContactsRepository {
     return contact;
   }
   public async findByEmail(email: string): Promise<Contact | undefined> {
-    throw new Error('Method not implemented.');
+    const contact = await this.repository.findOne({ email });
+
+    return contact;
   }
   public async findById(id: string): Promise<Contact | undefined> {
-    throw new Error('Method not implemented.');
+    const contact = await this.repository.findOne({ id });
+
+    return contact;
+  }
+
+  public async findRegistered(
+    firstName?: string,
+    email?: string,
+  ): Promise<Contact | undefined> {
+    const contactQuery = this.repository.createQueryBuilder('find');
+
+    if (firstName) {
+      contactQuery.where('firstName LIKE :firstName', {
+        firstName: `%${firstName}%`,
+      });
+    }
+
+    if (email) {
+      contactQuery.where('email LIKE :email', { email: `%${email}%` });
+    }
+
+    const contact = contactQuery.getOne();
+
+    return contact;
   }
 }
 
