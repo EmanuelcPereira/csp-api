4f541daf2dc2049376a851abe6b5e8d2da7c5259
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-07-28 22:47:06 -0300

    feat: add contacts routes

--- src/modules/contacts/infra/http/routes/contacts.routes.ts
+++ src/modules/contacts/infra/http/routes/contacts.routes.ts
@@ -6,5 +6,6 @@ const contactsRouter = Router();
 const contactsController = new ContactController();
 
 contactsRouter.post('/', contactsController.create);
+contactsRouter.get('/', contactsController.ListContact);
 
 export default contactsRouter;
