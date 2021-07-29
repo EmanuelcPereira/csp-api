11d8adaa5c0de0e12e052a23e6a2b181f22fd1a2
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-07-28 22:48:14 -0300

    refactor: add phone repository to save phone data

--- src/modules/contacts/services/CreateContactService.ts
+++ src/modules/contacts/services/CreateContactService.ts
@@ -1,35 +1,52 @@
+import IPhonesRepository from '@modules/contacts/repositories/IPhonesRepository';
 import { inject, injectable } from 'tsyringe';
+import { v4 as uuid } from 'uuid';
 
 import AppError from '@shared/errors/AppError';
 
-import ICreateContactDTO from '../dtos/ICreateContactDTO';
-import Contact from '../infra/typeorm/entities/Contact';
 import IContactsRepository from '../repositories/IContactsRepository';
+
+interface IRequest {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phones: string;
+}
 
 @injectable()
 class CreateContactService {
   constructor(
     @inject('ContactsRepository')
     private contactRepository: IContactsRepository,
+    @inject('PhonesRepository')
+    private phonesRepository: IPhonesRepository,
   ) { }
   public async execute({
     firstName,
     lastName,
     email,
-  }: ICreateContactDTO): Promise<Contact> {
+    phones,
+  }: IRequest): Promise<void> {
+    const id = uuid();
+    const newPhone = phones.split(',');
     const contactExists = await this.contactRepository.findByEmail(email);
 
     if (contactExists) {
-      throw new AppError('Driver already registered');
+      throw new AppError('Contact already registered');
     }
-
-    const contact = this.contactRepository.create({
+    this.contactRepository.create({
+      id,
       firstName,
       lastName,
       email,
     });
-
-    return contact;
+    const contact_id = id;
+    newPhone.forEach(phone => {
+      this.phonesRepository.createPhone({
+        contact_id,
+        phone,
+      });
+    });
   }
 }
 
